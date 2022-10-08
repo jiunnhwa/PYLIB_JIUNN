6e85c0bda9f7d790927b0517a13456981cb7cef6
jiunnhwa <zeonchan01@gmail.com>
2022-10-08 23:11:49 +0800

    first example

--- namedtuple.py
+++ namedtuple.py
@@ -1,1 +1,24 @@
-# https://realpython.com/python-namedtuple/#:~:text=Python's%20namedtuple%20was%20created%20to,code%20cleaner%20and%20more%20maintainable.
+"""
+https://realpython.com/python-namedtuple/#:~:text=Python's%20namedtuple%20was%20created%20to,code%20cleaner%20and%20more%20maintainable.
+https://death.andgravity.com/namedtuples
+
+"""
+
+
+from collections import namedtuple
+
+
+"""
+=============================================================
+"""
+Employee = namedtuple("Employee", "ID Name Role")
+John = Employee("001", "John", ["CEO"])
+
+print(John)
+print( id(John.Role))
+
+John.Role.append ("CTO")
+print(John)
+print( id(John.Role))
+
+# -------------------------------------------------------------
