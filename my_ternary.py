0b20382e1c91addd1f333c1f19fa5dfbb8bd24b6
jiunnhwa <57693037+jiunnhwa@users.noreply.github.com>
2022-10-26 15:56:39 +0800

    Update my_ternary.py

--- my_ternary.py
+++ my_ternary.py
@@ -7,6 +7,23 @@ https://flexiple.com/python/python-ternary/
 
 
 
+# ------------------------------------------------------------
+"""
+https://onecompiler.com/python/3yk6ezpct
+"""
+nice = True
+personality = ("mean", "nice")[nice]
+print("The stranger is", personality)
+
+is_nice = [True, False, None]
+# state = "nice" if is_nice else "not nice"
+# state = "none" if is_nice is None else "nice" if is_nice else "not nice"
+for x in is_nice:
+  state = "nice" if x else "not nice" if x is False else "none"
+  print(state)
+
+  
+# ------------------------------------------------------------
 text = """
 I came, I saw.
 """
