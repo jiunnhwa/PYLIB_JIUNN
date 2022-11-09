30a17079097538380a47727302393ccb971661ab
jiunnhwa <57693037+jiunnhwa@users.noreply.github.com>
2022-11-09 17:24:14 +0800

    Update my_list.py

--- my_list.py
+++ my_list.py
@@ -2,3 +2,9 @@
 https://therenegadecoder.com/code/how-to-write-a-list-comprehension-in-python/
 
 """
+
+"""
+https://www.datasciencelearner.com/nested-list-comprehension-python/
+flattened_list = [ ele for inner_list in outer_list for ele in inner_list]
+df_data = [ parse_to_cols(line) for grp in groups for line in grp]
+"""
