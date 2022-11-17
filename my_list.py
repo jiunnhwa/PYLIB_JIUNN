28901c4631352cfab23500adaf3475d99d843cd1
jiunnhwa <57693037+jiunnhwa@users.noreply.github.com>
2022-11-17 13:46:36 +0800

    Update my_list.py

--- my_list.py
+++ my_list.py
@@ -8,3 +8,22 @@ https://www.datasciencelearner.com/nested-list-comprehension-python/
 flattened_list = [ ele for inner_list in outer_list for ele in inner_list]
 df_data = [ parse_to_cols(line) for grp in groups for line in grp]
 """
+
+txt = """<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1"/>
+<title>Error 404 Not Found</title>
+</head>
+<body><h2>HTTP ERROR 404 Not Found</h2>
+<table>
+
+<tr><th>STATUS:</th><td>404</td></tr>
+<tr><th>MESSAGE:</th><td>Not Found</td></tr>
+<tr><th>SERVLET:</th><td>Stapler</td></tr>
+</table>
+</body>
+</html>
+"""
+
+print( any([x for x in ['window.location.replace','Not found','Not Found'] if x in txt]))
+ 
