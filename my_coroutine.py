f86c6bbc5aae4ebf2324c81884e3d9fd3b73024d
jiunnhwa <zeonchan01@gmail.com>
2022-12-11 16:28:34 +0800

    added files

---
+++ my_coroutine.py
@@ -0,0 +1,8 @@
+"""
+http://www.dabeaz.com/coroutines/index.html
+https://eli.thegreenplace.net/2009/08/29/co-routines-as-an-alternative-to-state-machines/ Co-routines are to state machines what recursion is to stacks
+https://polydorkic.hashnode.dev/a-finite-state-machine-fsm-implemented-in-python-via-coroutines
+https://arpitbhayani.me/blogs/fsm
+https://wiki.c2.com/?CoRoutine
+
+"""
