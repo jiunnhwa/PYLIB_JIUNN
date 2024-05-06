2ae25d4891cd0e0e9f549bd93bcdf37ee535784f
jiunnhwa <57693037+jiunnhwa@users.noreply.github.com>
2024-05-06 18:04:27 +0800

    Update my_file.py
    Add get_tempdir

--- my_file.py
+++ my_file.py
@@ -1,6 +1,24 @@
+'''
+
+https://safjan.com/mastering-temporary-files-and-directories-with-python-tempfile-module/
+https://stackoverflow.com/questions/847850/cross-platform-way-of-getting-temp-directory-in-python
+
+
+'''
+import os
+from pathlib import Path
+import platform
+import tempfile
+
+
+def get_tempdir():
+    tempdir = Path("/tmp" if platform.system() == "Darwin" else tempfile.gettempdir())
+    return tempdir
+
 
 def path_makedir(filename):
     os.makedirs(os.path.dirname(filename), exist_ok=True)
+
 
 def path_join(*args):
     return os.path.join(*args)
@@ -27,16 +45,21 @@ def file_appendtext(filename, text):
         
 def file_writetext(filename, text):
     """file write text"""
-    
     # makedirs(dirname(filename), exist_ok=True)
     with open(filename, "w") as file_ptr:
         file_ptr.write(text)
 
-      
+
 def print_to_file(data, filename="_run.txt", filepath=""):
-    """print to file"""
-    filepath = filepath if filepath else getdir_testlist_blwin32win32()
+    """print to file, becomes append if file exists"""
+    filepath = filepath if filepath else get_tempdir()
     filename = path_join(filepath, filename)
     with open(filename, "a") as file_ptr:
         print(data, file=file_ptr)
-      
+
+
+def UnitTest():
+    ''' selective unit tests '''
+    print(get_tempdir())
+    print_to_file("test print to temp dir")
+
